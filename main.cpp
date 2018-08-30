c5767f0570a08401535ebde4007a63257d1c4828
Max Morris <maxmorris2001@gmail.com>
2018-08-30 07:34:01 -0500

    finished CMakeLists file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <logicalaccess/dynlibrary/librarymanager.hpp>
+#include <logicalaccess/readerproviders/readerconfiguration.hpp>
+#include <logicalaccess/cards/chip.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, NFC!" << std::endl;
     return 0;
 }
